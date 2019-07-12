2cbebe5a29f445ea1da39b9bbdf1ced7aea34e01
Kalk <simon.d.mansson@gmail.com>
2019-07-12 20:37:19 +0200

    aaaa

--- src/Search.tsx
+++ src/Search.tsx
@@ -2,11 +2,11 @@
 import React from 'react';
 import { useState } from 'react';
 
-const Search: React.SFC = () => {
+const Search: React.FC = () => {
   const [ beverage, setBeverage ] = useState<string>('');
 
   return (
-    <form onSubmit={(e) => {e.preventDefault(); alert(beverage)}}>
+    <form onSubmit={(e) => {e.preventDefault(); }}>
         <input type="text" onChange={(e) => setBeverage(e.target.value)}/>
         <button type="submit">Search</button>
     </form>
