6684cd799c52d2f084dd2b8364fc60e0e0201c40
Kalk <simon.d.mansson@gmail.com>
2019-07-24 19:38:54 +0200

    Style search form

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Col, Row, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 export type OnSubmit = (beverage:string, location:string) => void;
 type SearchProps = {
@@ -10,24 +11,34 @@ export const Search: React.FunctionComponent<SearchProps> = ({callback}) => {
   const [ location, setLocation ] = useState<string>('');
 
   return (
-    <form onSubmit={(e) => {e.preventDefault(); callback(beverage, location); }}>
-        <label htmlFor="beverage-input">wut drink u look 4</label>
-        <input
-          id="beverage-input"
-          type="text"
-          required
-          onChange={(e) => setBeverage(e.target.value)}
-          autoFocus
-        />
-        <label htmlFor="location-input">Where u wanna look?</label>
-        <input
-          id="location-input"
-          type="text"
-          required
-          onChange={(e) => setLocation(e.target.value)}
-        />
-        <button type="submit">Search</button>
-    </form>
+    <Form onSubmit={(e) => {e.preventDefault(); callback(beverage, location); }}>
+      <Row>
+        <Col md={3}>
+          <FormGroup>
+            <Label htmlFor="beverage-input">Beverage</Label>
+            <Input
+              id="beverage-input"
+              type="text"
+              required
+              onChange={(e) => setBeverage(e.target.value)}
+              autoFocus
+            />
+          </FormGroup>
+        </Col>
+        <Col md={3}>
+          <FormGroup>
+            <Label htmlFor="location-input">Location</Label>
+            <Input
+              id="location-input"
+              type="text"
+              required
+              onChange={(e) => setLocation(e.target.value)}
+            />
+          </FormGroup>
+        </Col>
+      </Row>
+      <Button type="submit">Search</Button>
+    </Form>
   );
 }
 
